ba0cdf1a71417ef19a264e42bafbf69ba44cae10
Thoai Nguyen <nnthoai@gmail.com>
2025-03-28 16:12:37 +0700

    update code TaskDialog_5

--- 1_TaskDialog/Lession1_TaskDialog_5.cs
+++ 1_TaskDialog/Lession1_TaskDialog_5.cs
@@ -32,7 +32,7 @@ namespace RevitAPI_MEP
             }
 
             // End Code
-            // Kết thúc dòng code của TakeDialog_5
+            // End Code của TakeDialog_5
             return Result.Succeeded;
         }
     }
