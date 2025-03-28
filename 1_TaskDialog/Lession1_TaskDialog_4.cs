affb4ce330b0a14e5ed55cdee4db89a1f8c31cf3
Thoai Nguyen <121947896+nnthoai@users.noreply.github.com>
2025-03-28 13:14:44 +0700

    Add files via upload

--- 1_TaskDialog/Lession1_TaskDialog_4.cs
+++ 1_TaskDialog/Lession1_TaskDialog_4.cs
@@ -17,16 +17,21 @@ namespace RevitAPI_MEP
 
             td.MainInstruction = "Hiển thị các lựa chọn."; // Tiêu đề của nội dung.
             td.MainContent = "Bạn muốn thực hiện thao tác nào?"; // Nội dung chi tiết.
-            td.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, "Lựa chọn 1: Xử lý dữ liệu");
-            td.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, "Lựa chọn 2: Thoát mà không thay đổi");
+            td.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, "Lựa chọn 1: Xử lý dữ liệu 1");
+            td.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, "Lựa chọn 2: Xử lý dữ liệu 2");
+            td.AddCommandLink(TaskDialogCommandLinkId.CommandLink3, "Lựa chọn 3: Thoát mà không thay đổi");
 
             TaskDialogResult result = td.Show();
 
             if (result == TaskDialogResult.CommandLink1)
             {
-                TaskDialog.Show("Thông báo", "Bạn đã chọn Xử lý dữ liệu.");
+                TaskDialog.Show("Thông báo", "Bạn đã chọn Xử lý dữ liệu 1.");
             }
             else if (result == TaskDialogResult.CommandLink2)
+            {
+                TaskDialog.Show("Thông báo", "Bạn đã chọn Xử lý dữ liệu 2.");
+            }
+            else
             {
                 TaskDialog.Show("Thông báo", "Bạn đã chọn Thoát mà không thay đổi.");
             }
